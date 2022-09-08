4aeb7511852ae4a964f86bbc95e458f9b35babed
isaaccleopas <isaaccleopas@gmail.com>
2022-09-08 15:48:26 +0000

    C command to print

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * 
+ * puts - prints
+ *
+ * Return:is 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
