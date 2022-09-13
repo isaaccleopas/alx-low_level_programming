3b33723528c08fb77a01c2d1d355e63217df264b
isaaccleopas <isaaccleopas@gmail.com>
2022-09-13 11:46:22 +0000

    Task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - main function
+ *
+ * s: integer to make absolute
+ *
+ * Return: integer or integer * negative
+ */
+int _abs(int s)
+{
+	if (s > 0)
+		return (s);
+	else
+		return (n * -1);
+}
