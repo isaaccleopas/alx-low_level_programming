12be6d7450fd54297c855d36f7c168717f8d8f07
isaaccleopas <isaaccleopas@gmail.com>
2022-09-13 14:02:49 +0000

    Task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * add - functions that add integer
+ * 
+ * a: first integer
+ * 
+ * b: second integer
+ *
+ * Return: sum of both integers
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
