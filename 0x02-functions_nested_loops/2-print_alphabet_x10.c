9a0acca40989aac2f3b78059294b6a0b2449b38e
isaaccleopas <isaaccleopas@gmail.com>
2022-09-13 11:04:03 +0000

    Task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "alphabets.h"
+
+/**
+ * print_alphabet_x10 - prints lowercase alphabet x10
+ *
+ * Retun: Always returns 0
+ */
+void print_alphabet_x10(void)
+{
+	char = i;
+	char = j;
+	
+	for (i = 0; i < 10; i++;)
+	{
+		for (h = 'a'; h <= 'z'; h++)
+		{
+			_putchar(h);
+		}
+		_putchar("\n");
+	}
+}
