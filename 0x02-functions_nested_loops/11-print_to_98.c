1afa7e04c4078c1d2fd29b9dd92bdc06d8a7ab6e
isaaccleopas <isaaccleopas@gmail.com>
2022-09-13 14:30:41 +0000

    Task 11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,35 @@
+#include "main.c"
+#include <stdio.h>
+
+/**
+ * print_to_98 - main function
+ *
+ */
+void print_to_98(int n)
+{
+	int i;
+
+	if (n > 98)
+	{
+		for (i = n; i >= 98; i--)
+		{
+			printf("%d", i);
+			if (i != 98)
+			{
+				printf(",");
+			}
+		}
+	}
+	else
+	{
+		for (i = n; i <= 98; i++)
+		{
+			printf("%d", i);
+			if (i != 98)
+			{
+				printf(",");
+			}
+		}
+	}
+	printf('\n');
+}
