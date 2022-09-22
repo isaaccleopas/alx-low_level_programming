baf57352a9527cb911719ad6cd84ff9c00d1c8eb
isaaccleopas <isaaccleopas@gmail.com>
2022-09-22 13:59:23 +0000

    Task 100

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -74,4 +74,7 @@ char *rot13(char *);
  * @n: number to print to character
  */
 void print_number(int n);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void print_buffer(char *b, int size);
+
 #endif
