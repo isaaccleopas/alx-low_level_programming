decc38071ef61ff475fd61e4a668ac073eae2f73
isaaccleopas <isaaccleopas@gmail.com>
2022-09-22 11:38:26 +0000

    Task 0

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,61 +1,30 @@
-#ifndef MAIN_H_
-#define MAIN_H_
-#include <stdio.h>
-/**
- * leet - switches characters to 1337
- * @s: string to switch
- *
- * Return: returns character
- */
-char *leet(char *)
-/**
- * cap_string - string to capitalize;
- * @s: string to capitalize
- * Return: return the string
- */
-char *cap_string(char *)
-/**
- * string_toupper - makes lower to upper
- * @s: character to make upper
- * Return: character value
- */
-char *string_toupper
-/**
- * reverse_array - reverses an array
- * @a: array to reverse
- * @n: how many elements
- **/
-void reverse_array(int *a, int n)
-/**
- * _strcmp - compares strings
- *
- * @s1: first string
- * @s2: second string to compare
- * Return: int value
- */
-int _strcmp(char *s1, char *s2)
-/**
- * _strncpy - function to copy
- * @dest: destination to copy
- * @src: source to copy
- * @n: times to copy
- * Return: return char value
- */
-char *_strncpy(char *dest, char *src, int n)
-/**
- * _strcat - concats strings
- * @dest: destination of string
- * @src: source of string to concat
- *
- * Return: returns char value
- */
-char *_strcat(char *dest, char *src)
-/**
- * _strncat - concat string at n times
- * @dest: destination source
- * @src: source of array
- * @n: n amount of times
- * Return: return char value
- */
-char *_strncat(char *dest, char *src, int n)
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
+
+
+char *_strcat(char *dest, char *src);
+
+char *_strncat(char *dest, char *src, int n);
+
+char *_strncpy(char *dest, char *src, int n);
+
+int _strcmp(char *s1, char *s2);
+
+void reverse_array(int *a, int n);
+
+char *string_toupper(char *);
+
+char *cap_string(char *);
+
+char *leet(char *);
+
+char *rot13(char *);
+
+void print_number(int n);
+
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+
+void print_buffer(char *b, int size);
 #endif
