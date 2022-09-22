5be8eb843615758bfe8afa141adaabd25ad8d8db
isaaccleopas <isaaccleopas@gmail.com>
2022-09-22 13:48:22 +0000

    Task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,9 +1,26 @@
 #include "main.h"
 /**
- * cap_string - capitalizes all words in a string
- * @s: string to capitalize
- *
- * Return: address of s
+ * _indexOf - returns boolean if special  character
+ * @a: character to return
+ * Return: true or false
+ */
+int _indexOf(char a)
+{
+	int i;
+	char capArr[13] = {'\n', '\t', ' ', '.', ',', ';', ',', '!', '?', '(',
+')', '{', '}'};
+
+	for (i = 0; i < 13; i++)
+	{
+		if (capArr[i] == a)
+			return (1);
+	}
+	return (0);
+}
+/**
+ * cap_string - capitalizes the string
+ * @s: string
+ * Return: the string capitalized
  */
 char *cap_string(char *s)
 {
