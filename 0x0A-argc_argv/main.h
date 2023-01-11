c36593fe85e28576037705effc796cd19b3991d1
isaaccleopas <isaaccleopas@gmail.com>
2023-01-11 06:34:00 +0000

    advanced task added

--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -1,6 +1,12 @@
 #ifndef MAIN_H_
 #define MAIN_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
 int _putchar(char c);
 int main(int argc, char *argv[]);
 int stringcheck(char *s);
+
 #endif
