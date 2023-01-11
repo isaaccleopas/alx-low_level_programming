c1da1156cdabc94894ef0a774149c48ec959ba05
isaaccleopas <isaaccleopas@gmail.com>
2023-01-11 05:56:21 +0000

    101-keygen.c added

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,46 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <string.h>
 
-int main(void) 
-{
-	char password[84];
-	int index = 0, sum = 0, diff_half, diff_half2;
-	
-	srand(time(0));
-	
-	while (sum < 2772)
-	{
-		password[index] = 33 + rand() % 94;
-		sum += password[index++];
-	}
-	password[index] = '\0';
-	if (sum != 2772)
-	{
-		diff_half1 = (sum - 2772) / 2;
-		diff_half2 = (sum - 2772) / 2;
-		
-		if ((sum - 2772) % 2 != 0)
-					diff_half1++;
-		for  (index = 0; password[index]; index++)
-						{
-							if (password[index] >= (33 + diff_half1))
-							{
-								password[index] -= diff_half1;
+int main() {
+    char password[7];
+    char charset[] = "0123456789";
+    int len_charset = strlen(charset);
+    srand(time(NULL));
 
-								break;
-							}
-						}
-		for (index = 0; password[index]; index++)
-		{
-			if (password[index] >= (33 + diff_half2))
-
-				password[index] -= diff_half2;
-				break;
-			}
-
-		}
-	}
-	printf("%s", password);
-	return 0;
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 6; j++) {
+            int random_index = rand() % len_charset;
+            password[j] = charset[random_index];
+        }
+        password[6] = '\0';
+        printf("%s\n", password);
+    }
+    return 0;
 }
