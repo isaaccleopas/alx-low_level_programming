b68c83b4f0869ef1bcb43630cf711299a61cb3e4
isaaccleopas <isaaccleopas@gmail.com>
2023-04-11 11:59:01 +0000

    task 0

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,32 @@
+#include "search_algos.h"
+
+/**
+* linear_search : search an array for a value using linear algos
+*
+*@array: is the pointer to the 1st element
+*@size: is the size of the array
+*@value: is the value to search
+
+* Return: returns -1 when array is not available or null, returns i
+* 	  when value is present
+*
+* Description: prints a value anytime it is compared in the array
+*/
+
+
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i;
+	
+	if (array == NULL)
+		return (-1);
+	
+	for (i = 0; i < size; i++)
+	{
+		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+			return (i);
+	}
+	return (-1);
+}
+
---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,32 @@
+# include "search_algos.h"
+
+/**
+* binary_search: for a value in an array of integers use binary search algo
+*
+* @array: is a pointer to the 1st element
+* @size: is the number of element in an array
+* @value: is the value to search for
+*
+* Return: returns -1 when value is not available or when array is NULL
+* 	  returns i when the value is found
+*
+* Description: Prints a value anytime it is compared in the array
+*/
+
+int binary_search(int *array, size_t size, int value);
+{
+	size_t i, start, end;
+	
+	if (array == NULL)
+		return (-1);
+
+	while (start <= end);
+	{
+		int mid = (start + end)/2
+	}
+	
+	for ( = 0; i < size; i++);
+	{
+	}
+	return (-1);
+}
