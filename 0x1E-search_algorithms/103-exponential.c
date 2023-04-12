06d0ac7b066c7f88596a54c9d8fa1c0aa7c77b2e
isaaccleopas <isaaccleopas@gmail.com>
2023-04-12 09:27:17 +0000

    exponential algo correction

--- 0x1E-search_algorithms/103-exponential.c
+++ 0x1E-search_algorithms/103-exponential.c
@@ -1,57 +1,52 @@
 #include "search_algos.h"
 
 /**
- * exponential_search - Searches for a value in a sorted array of integers
+ * exponential_search - searches for a value in a sorted array of integers
  * using the Exponential search algorithm
- * @array: A pointer to the first element of the array to search in
- * @size: The number of elements in the array
- * @value: The value to search for
+ * @array: pointer to the first element of the array to search in
+ * @size: number of elements in the array
+ * @value: value to search for
  *
- * Return: The index of the value in the array, or -1 if not found
+ * Return: first index where value is located or -1 if value is not present
+ * or if array is NULL
  */
 int exponential_search(int *array, size_t size, int value)
 {
-    size_t bound = 1, left, right, prev;
+	size_t bound = 1, left = 0, right = size - 1, mid;
 
-    if (array == NULL)
-        return (-1);
+	if (array == NULL || size == 0)
+		return (-1);
 
-    while (bound < size && array[bound] < value)
-    {
-        printf("Value checked array[%lu] = [%d]\n", bound, array[bound]);
-        prev = bound;
-        bound *= 2;
-    }
+	while (bound < size && array[bound] < value)
+	{
+		printf("Value checked array[%lu] = [%d]\n", bound, array[bound]);
+		left = bound;
+		bound *= 2;
+	}
 
-    left = prev;
-    right = bound < size ? bound : size - 1;
+	if (bound >= size)
+		right = size - 1;
+	else
+		right = bound;
 
-    printf("Value found between indexes [%lu] and [%lu]\n", left, right);
+	printf("Value found between indexes [%lu] and [%lu]\n", left, right);
 
-    while (left <= right)
-    {
-        size_t mid = (left + right) / 2;
+	while (left <= right)
+	{
+		printf("Searching in array: ");
+		for (mid = left; mid < right; mid++)
+			printf("%d, ", array[mid]);
+		printf("%d\n", array[mid]);
+		if (array[left] == value)
+			return (left);
+		mid = (left + right) / 2;
+		if (array[mid] < value)
+			left = mid + 1;
+		else if (array[mid] > value)
+			right = mid - 1;
+		else
+			return (mid);
+	}
 
-        printf("Searching in array: ");
-        for (size_t i = left; i <= right; i++)
-            printf("%d%s", array[i], i == right ? "\n" : ", ");
-
-        if (array[mid] < value)
-        {
-            printf("Value checked array[%lu] = [%d]\n", mid, array[mid]);
-            left = mid + 1;
-        }
-        else if (array[mid] > value)
-        {
-            printf("Value checked array[%lu] = [%d]\n", mid, array[mid]);
-            right = mid - 1;
-        }
-        else
-        {
-            printf("Value checked array[%lu] = [%d]\n", mid, array[mid]);
-            return (mid);
-        }
-    }
-
-    return (-1);
+	return (-1);
 }
