74b2d1bf9bf4f3e792d1487d111babba9a94de2c
isaaccleopas <isaaccleopas@gmail.com>
2023-04-12 07:00:21 +0000

    task 0 corrected

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,17 +1,17 @@
 #include "search_algos.h"
 
 /**
-* linear_search : search an array for a value using linear algos
-*
-* @array: is the pointer to the 1st element
-* @size: is the size of the array
-* @value: is the value to search
-*
-* Return: returns -1 when array is not available or null, returns i
-* 	  when value is present
-*
-* Description: prints a value anytime it is compared in the array
-*/
+ * linear_search : search an array for a value using linear algos
+ *
+ * @array: is the pointer to the 1st element
+ * @size: is the size of the array
+ * @value: is the value to search
+ *
+ * Return: returns -1 when array is not available or null, returns i
+ * 	  when value is present
+ *
+ * Description: prints a value anytime it is compared in the array
+ */
 
 
 int linear_search(int *array, size_t size, int value)
