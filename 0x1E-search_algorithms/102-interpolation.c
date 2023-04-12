2457aa20d1a352d3ab546334faacbcb1ba0ab5f9
isaaccleopas <isaaccleopas@gmail.com>
2023-04-12 08:56:16 +0000

    interpolation algo

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -1,29 +1,27 @@
-#include "search_algos.h"
 #include <stdio.h>
+#include "search_algos.h"
 
 /**
  * interpolation_search - Searches for a value in a sorted array of integers
- *                        using the Interpolation search algorithm.
- * @array: A pointer to the first element of the sorted array to search.
- * @size: The number of elements in the array.
- * @value: The value to search for.
+ *                         using the Interpolation search algorithm
+ * @array: Pointer to the first element of the array to search in
+ * @size: Number of elements in array
+ * @value: Value to search for
  *
- * Return: If the value is not present or the array is NULL, -1.
- *         Otherwise, the first index where the value is located.
+ * Return: The first index where value is located, or -1 if not found or if
+ *         array is NULL
  */
 int interpolation_search(int *array, size_t size, int value)
 {
-	size_t low = 0, high = size - 1;
+	size_t low = 0, high = size - 1, pos;
 
 	if (array == NULL)
 		return (-1);
 
 	while (low <= high && value >= array[low] && value <= array[high])
 	{
-		size_t pos = low + (((double)(high - low) /
-				      (array[high] - array[low])) *
-				    (value - array[low]));
-
+		pos = low + (((double)(high - low) / (array[high] - array[low])) *
+			(value - array[low]));
 		printf("Value checked array[%lu] = [%d]\n", pos, array[pos]);
 
 		if (array[pos] == value)
