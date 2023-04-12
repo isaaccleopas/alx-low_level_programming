a40bd7d3e0bbed54ae67582095a4081b89678580
isaaccleopas <isaaccleopas@gmail.com>
2023-04-12 08:11:32 +0000

    binary algo

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,45 +1,38 @@
 #include "search_algos.h"
 
 /**
- * binary_search - Searches for a value in a sorted array
- *                 of integers using binary search.
- * @array: A pointer to the first element of the array to search.
- * @size: The number of elements in the array.
- * @value: The value to search for.
+ * binary_search - Searches for a value in a sorted array of integers using
+ * the Binary search algorithm
+ * @array: Pointer to the first element of the array to search in
+ * @size: Number of elements in array
+ * @value: Value to search for
  *
- * Return: If the value is not present or the array is NULL, -1.
- *         Otherwise, the index where the value is located.
- *
- * Description: Prints the [sub]array being searched after each change.
+ * Return: Index where value is located, or -1 if value is not present
+ * or array is NULL
  */
-
 int binary_search(int *array, size_t size, int value)
 {
-	size_t i;
-	int start = 0;
-	int end = size - 1;
-	int mid;
+	size_t left = 0, right = size - 1, mid;
 
 	if (array == NULL)
 		return (-1);
 
-	while (start <= end)
+	while (left <= right)
 	{
-		mid = (start + end) / 2;
 		printf("Searching in array: ");
-		for (i = (size_t)start; i <= (size_t)end; i++){	
-			printf("%d ", array[i]);
-		}
-		printf("\n");
+		for (mid = left; mid < right; mid++)
+			printf("%d, ", array[mid]);
+		printf("%d\n", array[mid]);
 
-		if (array[mid] == value) {
+		mid = (left + right) / 2;
+
+		if (array[mid] == value)
 			return (mid);
-		} else if (array[mid] < value) {
-			start = mid + 1;
-		} else {
-			end = mid - 1;
-		}
 
+		if (array[mid] < value)
+			left = mid + 1;
+		else
+			right = mid - 1;
 	}
 
 	return (-1);
--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -14,21 +14,22 @@ int jump_search(int *array, size_t size, int value)
 {
     size_t end = sqrt(size);
     size_t start = 0;
+    size_t curr = 0;
 
     if (array == NULL)
 	    return (-1);
 
     while (array[start] < value)
     {
-        printf("Value checked array[%ld] = [%d]\n", start, array[prev]);
+        printf("Value checked array[%ld] = [%d]\n", start, array[start]);
 
         start = end;
 
         if (start >= size)
-            break;
+		return 0;
     }
 
-    size_t curr = start - end;
+    curr = start - end;
     printf("Value found between indexes [%ld] and [%ld]\n", curr, start);
 
     while (array[curr] < value)
